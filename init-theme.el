1102958b9d3a8685af47bb9c9a83afedde349af1
fengjiantao <502359010@qq.com>
2013-11-08 09:22:11 +0800

    refresh my site-lisp

--- init-theme.el
+++ init-theme.el
@@ -1,11 +1,12 @@
 ;;----------------------------------------------------------------------------
 ;; define my perfer theme 
 ;;----------------------------------------------------------------------------
-;;(color-theme-solarized-dark)
+(color-theme-solarized-dark)
 ;;(color-theme-solarized-light)
-(color-theme-molokai)
+;;(color-theme-molokai)
 
 (color-theme-initialize)
+;;(color-theme-deep-blue)
 ;;(color-theme-classic)
 ;;(color-theme-ld-dark)
 
