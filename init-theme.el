832118e630903bcbb19598e17f9a098b113dbc7b
fengjiantao <502359010@qq.com>
2014-01-05 13:44:09 +0800

    delete html helper mode and update theme

--- init-theme.el
+++ init-theme.el
@@ -1,13 +1,15 @@
 ;;----------------------------------------------------------------------------
 ;; define my perfer theme 
 ;;----------------------------------------------------------------------------
-(color-theme-solarized-dark)
+;;(color-theme-solarized-dark)
 ;;(color-theme-solarized-light)
 ;;(color-theme-molokai)
 
 (color-theme-initialize)
 ;;(color-theme-deep-blue)
 ;;(color-theme-ld-dark)
+;;(color-theme-snow)
+(color-theme-xp)
 
 ;;(color-theme-molokai)
 
