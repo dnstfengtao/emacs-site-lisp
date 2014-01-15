33523d0809e2e79da4fae0c3d60abf1bb53954dc
fengjiantao <502359010@qq.com>
2014-01-15 07:38:58 +0000

    add some new file and compile all file

--- init-util.el
+++ init-util.el
@@ -4,7 +4,7 @@
   (let ((beg (line-beginning-position))
         (end (line-end-position arg)))
     (copy-region-as-kill beg end))
-)
+  )
 
 (defun copy-word (&optional arg)
   "Copy words as point"
@@ -12,7 +12,7 @@
   (let ((beg (progn (if (looking-back "[a-zA-Z0-9]" 1) (backward-word 1)) (point)))
         (end (progn (forward-word arg) (point))))
     (copy-region-as-kill beg end))
-)
+  )
 
 (defun copy-paragraph (&optional arg)
   "Copy paragraphes at point"
@@ -20,4 +20,13 @@
   (let ((beg (progn (backward-paragraph 1) (point)))
         (end (progn (forward-paragraph arg) (point))))
     (copy-region-as-kill beg end))
-)
+  )
+
+(defun qiang-comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command. If no region is selected and current line is not blank and we are not at the end of the line, then comment current line. Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'qiang-comment-dwim-line)
