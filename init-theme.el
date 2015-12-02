305eae066bda8cf7a8a693ca0887248b1f17bd6c
finleyfeng <fengjiantao@hotmail.com>
2015-12-02 21:35:44 +0800

    set up my initial new emacs configuration repository

---
+++ init-theme.el
@@ -0,0 +1,8 @@
+;;----------------------------------------------------------------------------
+;; This is the color theme initial configuration file
+;; I install the theme package zenburn 
+;; This package is require the color-theme package 6.6.1
+;; so we can only require this package to use the theme package.
+;;---------------------------------------------------------------------------- 
+(require 'color-theme-zenburn)
+(color-theme-zenburn)
