578fbff5225b1f3bdb5b11978c1d46f5184dd24d
finleyfeng <fengjiantao@hotmail.com>
2016-02-09 20:46:41 +0800

    change company mode initial file

--- init-company.el
+++ init-company.el
@@ -1,1 +1,7 @@
 (require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; define key binding
+(setq company-backends (delete 'company-semantic company-backends))
+(define-key c-mode-map  [(C-tab)] 'company-complete)
+(define-key c++-mode-map  [(C-tab)] 'company-complete)
